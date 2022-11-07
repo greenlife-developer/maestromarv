923d60dfb98b9e15904e3a2ea9ffdc1be0f9acd8
Afolabi Opeyemi <yemijoshua81@gmail.com>
2022-11-07 14:12:09 +0100

    adding nav background

--- client/src/components/homepage/Navigation.jsx
+++ client/src/components/homepage/Navigation.jsx
@@ -5,7 +5,7 @@ import logo from "../images/logo.png";
 
 export default function Navigation() {
   const [navBackground, setNavBackground] = useState(false);
-  const [icon, setIcon] = useState(true);
+  // const [icon, setIcon] = useState(true);
 
   window.onscroll = function () {
     if (window.scrollY > window.innerHeight) {
@@ -16,32 +16,15 @@ export default function Navigation() {
     }
   };
 
-  function handleDropClick(e) {
-    if (icon) {
-      setIcon(false);
-      document.getElementById("dropDownContent").style.display = "block";
-      document.getElementById("dropDownContent").style.transform =
-        "translateY(0)";
-    } else {
-      setIcon(true);
-      document.getElementById("dropDownContent").style.display = "none";
-      document.getElementById("dropDownContent").style.transform =
-        "translateY(0)";
-    }
-  }
-
-  // const style = { "--i": .15 } as React.CSSProperties
-
   return (
     <>
-      <header>
+      <header style={{backgroundColor: navBackground ? "rgba(0, 0, 0, 1)" : "rgba(0, 0, 0, .3)" }}>
         <div className="container1">
           <input type="checkbox" name="" id="check" />
 
           <div className="logo-container">
             <img
               src={logo}
-              // style={{ borderRadius: "50%" }}
               height="50px"
               alt=""
             />
